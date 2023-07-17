f1cab13a49a75db59e337c567fa43e0fa8894289
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2023-07-17 13:21:01 +0900

    feat: Add current location on map screen

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,17 +1,39 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 
-class MapScreen extends StatelessWidget {
-  String MapTileURL = (dotenv.env['SERVER_URL'] as String) +
-      (dotenv.env['MAP_TILE_ENDPOINT'] as String);
+class MapScreen extends StatefulWidget {
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late FollowOnLocationUpdate _followOnLocationUpdate;
+  late StreamController<double?> _followCurrentLocationStreamController;
 
   final mapController = MapController();
 
-  MapScreen({Key? key}) : super(key: key);
+  String MapTileURL = (dotenv.env['SERVER_URL'] as String) +
+      (dotenv.env['MAP_TILE_ENDPOINT'] as String);
+
+  @override
+  void initState() {
+    super.initState();
+    _followOnLocationUpdate = FollowOnLocationUpdate.never;
+    _followCurrentLocationStreamController = StreamController<double?>();
+  }
+
+  @override
+  void dispose() {
+    _followCurrentLocationStreamController.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +54,11 @@ class MapScreen extends StatelessWidget {
         ),
         children: [
           TileLayer(urlTemplate: MapTileURL + '{z}/{x}/{y}.png'),
+          CurrentLocationLayer(
+            followOnLocationUpdate: _followOnLocationUpdate,
+            followCurrentLocationStream:
+                _followCurrentLocationStreamController.stream,
+          ),
         ],
       ),
     );
