7d47a30c5cd2858ff4360efe01bc0239e5a46e80
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2023-07-25 19:05:22 +0900

    design: Add AppColors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF1B4426);
+  static const primaryDark = Color(0xFF0A2F19);
+  static const text = Color(0xFF3D3D3D);
+  static const textInactive = Color(0xFF959595);
+}
